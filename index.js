58eb845f39fd057e378095b26a9da544ec3e5c10
Sara D'Arce <saradarce@gmail.com>
2024-01-16 14:12:59 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Required External Modules
+
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+
+/**
+ * Server Activation
+ */
